c1cb1a9fd9df248f0028809d735071914c40ee9f
Andrew Gacek <andrew.gacek@gmail.com>
2012-10-26 12:58:12 -0500

    Slice variable declarations

--- src/jkind/slicing/Slicer.java
+++ src/jkind/slicing/Slicer.java
@@ -7,12 +7,13 @@ import java.util.Set;
 
 import jkind.lustre.Equation;
 import jkind.lustre.Node;
+import jkind.lustre.VarDecl;
 
 public class Slicer {
 	public static Node slice(Node node) {
 		DependencyMap depMap = new DependencyMap(node);
 		Set<String> keep = getPropertyDependencies(node, depMap);
-		return keepOnlyListedEquations(node, keep);
+		return sliceByKeep(node, keep);
 	}
 
 	private static Set<String> getPropertyDependencies(Node node, DependencyMap depMap) {
@@ -23,13 +24,31 @@ public class Slicer {
 		return keep;
 	}
 	
-	private static Node keepOnlyListedEquations(Node node, Set<String> keep) {
-		List<Equation> slicedEquations = new ArrayList<Equation>();
-		for (Equation eq : node.equations) {
-			if (keep.contains(eq.id)) {
-				slicedEquations.add(eq);
+	private static Node sliceByKeep(Node node, Set<String> keep) {
+		List<VarDecl> inputs = sliceVarDecls(node.inputs, keep);
+		List<VarDecl> outputs = sliceVarDecls(node.outputs, keep);
+		List<VarDecl> locals = sliceVarDecls(node.locals, keep);
+		List<Equation> equations = sliceEquations(node.equations, keep);
+		return new Node(inputs, outputs, locals, equations, node.properties);
+	}
+
+	private static List<VarDecl> sliceVarDecls(List<VarDecl> decls, Set<String> keep) {
+		List<VarDecl> sliced = new ArrayList<VarDecl>();
+		for (VarDecl decl : decls) {
+			if (keep.contains(decl.id)) {
+				sliced.add(decl);
 			}
 		}
-		return new Node(node.inputs, node.outputs, node.locals, slicedEquations, node.properties);
+		return sliced;
+	}
+
+	private static List<Equation> sliceEquations(List<Equation> equations, Set<String> keep) {
+		List<Equation> sliced = new ArrayList<Equation>();
+		for (Equation eq : equations) {
+			if (keep.contains(eq.id)) {
+				sliced.add(eq);
+			}
+		}
+		return sliced;
 	}
 }
