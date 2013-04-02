441a5c25e8adaa2fa21875a44ba22cd78cb76780
Andrew Gacek <andrew.gacek@gmail.com>
2013-04-02 15:17:55 -0500

    Fix bug in slicing of assertions

--- src/jkind/slicing/DependencyMap.java
+++ src/jkind/slicing/DependencyMap.java
@@ -2,6 +2,7 @@ package jkind.slicing;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Stack;
@@ -17,7 +18,17 @@ public class DependencyMap {
 
 	public DependencyMap(Node node) {
 		this.map = new HashMap<String, Set<String>>();
+
 		computeOneStepDependencies(node);
+		closeDependencies();
+
+		/*
+		 * Assertions cause everything they (transitively) touch to be related
+		 * so we must handle them after computing the initial dependency map.
+		 * Then we must compute the closure again using dependency information
+		 * gained from assertions.
+		 */
+		analyzeAssertions(node.assertions);
 		closeDependencies();
 	}
 
@@ -25,22 +36,11 @@ public class DependencyMap {
 		for (VarDecl input : node.inputs) {
 			map.put(input.id, new HashSet<String>());
 		}
-		
+
 		for (Equation eq : node.equations) {
 			Set<String> deps = IdExtractorVisitor.getIds(eq.expr);
 			for (IdExpr idExpr : eq.lhs) {
 				map.put(idExpr.id, deps);
-			}
-		}
-
-		for (Expr assertion : node.assertions) {
-			Set<String> deps = IdExtractorVisitor.getIds(assertion);
-			for (String id : deps) {
-				if (map.containsKey(id)) {
-					map.get(id).addAll(deps);
-				} else {
-					map.put(id, new HashSet<String>(deps));
-				}
 			}
 		}
 	}
@@ -58,7 +58,7 @@ public class DependencyMap {
 		closure.add(root);
 		Stack<String> todo = new Stack<String>();
 		todo.push(root);
-		
+
 		while (!todo.empty()) {
 			String curr = todo.pop();
 			if (map.containsKey(curr)) {
@@ -71,6 +71,19 @@ public class DependencyMap {
 			}
 		}
 		return closure;
+	}
+
+	private void analyzeAssertions(List<Expr> assertions) {
+		for (Expr assertion : assertions) {
+			Set<String> deps = new HashSet<String>();
+			for (String id : IdExtractorVisitor.getIds(assertion)) {
+				deps.addAll(map.get(id));
+			}
+
+			for (String id : deps) {
+				map.put(id, new HashSet<String>(deps));
+			}
+		}
 	}
 
 	public Set<String> get(String id) {
