9e454bb2145bc943010dda7922be28d289951cde
Andrew Gacek <andrew.gacek@gmail.com>
2014-04-05 20:46:33 -0500

    Code clean up

--- jkind/src/jkind/translation/tuples/FlattenTuples.java
+++ jkind/src/jkind/translation/tuples/FlattenTuples.java
@@ -1,14 +1,13 @@
 package jkind.translation.tuples;
 
 import jkind.lustre.Node;
-import jkind.lustre.visitors.ExprMapVisitor;
 
 /**
  * Flatten all tuple expressions via expansion.
  * 
  * Assumption: All node calls have been inlined.
  */
-public class FlattenTuples extends ExprMapVisitor {
+public class FlattenTuples {
 	public static Node node(Node node) {
 		node = LiftTuples.node(node);
 		node = FlattenTupleComparisons.node(node);
